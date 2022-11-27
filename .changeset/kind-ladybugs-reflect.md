0505e0721e3e3dbb7e9fbf51ef07d7798064f173
ang <wang3ang@163.com>
2022-11-27 14:19:04 +0800

    fix: copy prefix dot file

---
+++ .changeset/kind-ladybugs-reflect.md
@@ -0,0 +1,7 @@
+---
+"@wa-dev/cli-shared-utils": major
+"@wa-dev/cli": major
+"@wa-dev/cli-service": major
+---
+
+change org
--- packages/cli-service/generator/index.js
+++ packages/cli-service/generator/index.js
@@ -9,6 +9,11 @@ module.exports = (api) => {
       react: "^18.2.0",
       "react-dom": "^18.2.0",
     },
+    devDependencies: {
+      "@types/react": "^18.0.25",
+      "@types/react-dom": "^18.0.9",
+      "@wa-dev/cli-service": "@1.0.0",
+    },
     browserslist: [">1%", "not dead", "not op_mini all"],
   });
 };
