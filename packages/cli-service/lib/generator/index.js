d3b1ca9c80587fe7313d422a9e720335cfca1f43
ang <wang3ang@163.com>
2022-11-27 19:34:10 +0800

    fix: change devDependencies to dependencies

--- packages/cli-service/lib/generator/index.js
+++ packages/cli-service/lib/generator/index.js
@@ -12,7 +12,8 @@ module.exports = (api) => {
     devDependencies: {
       "@types/react": "^18.0.25",
       "@types/react-dom": "^18.0.9",
-      "@wa-dev/cli-service": "@1.0.0",
+      "@wa-dev/cli-service": "2.0.0-beat.5",
+      typescript: "^4.9.3",
     },
     browserslist: [">1%", "not dead", "not op_mini all"],
   });
